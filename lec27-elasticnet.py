7ecea9fc9f638f0329334c43e60e2667a8a64207
issactoast <issactoast@gmail.com>
2025-09-03 10:58:17 +0900

    수치형, 범주형 학습 완료 - elastic net

--- lec27-elasticnet.py
+++ lec27-elasticnet.py
@@ -4,27 +4,78 @@ import pandas as pd
 train_df = pd.read_csv('./data/houseprice/train.csv')
 test_df = pd.read_csv('./data/houseprice/test.csv')
 
-train_df = train_df.select_dtypes(include=['number'])
+# train_df = train_df.select_dtypes(include=['number'])
 
-# 결측치 제거 (간단히 처리)
-train_df = train_df.dropna()
+num_columns = train_df.select_dtypes(include=['number']).columns
+cat_columns = train_df.select_dtypes(include=['object']).columns
+
+# 결측치 채우기 (간단히 처리)
+# train_df = train_df.dropna()
+from sklearn.impute import SimpleImputer
+freq_impute = SimpleImputer(strategy='most_frequent')
+mean_impute = SimpleImputer(strategy='mean')
+
+train_df[cat_columns] = freq_impute.fit_transform(train_df[cat_columns])
+train_df[num_columns] = mean_impute.fit_transform(train_df[num_columns])
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+onehot = OneHotEncoder(handle_unknown='ignore', 
+                       sparse_output=False).set_output(transform="pandas")
+std_scaler = StandardScaler().set_output(transform="pandas")
+
+train_df_cat = onehot.fit_transform(train_df[cat_columns])
+train_df_num = std_scaler.fit_transform(train_df[num_columns])
+
+train_df_all = pd.concat([train_df_cat,
+                          train_df_num], axis = 1)
 
 # 독립변수(X)와 종속변수(y) 분리
-X_train = train_df.drop(columns='SalePrice')
+X_train = train_df_all.drop(columns='SalePrice')
 y_train = train_df['SalePrice']
 
 from sklearn.linear_model import ElasticNet
+import numpy as np
 
-elastic = ElasticNet(alpha= 0.1,
-                     l1_ratio= 0.6)
-elastic.fit(X_train, y_train)
+elastic = ElasticNet()
+elastic_params = {'alpha' : np.arange(0.1, 1, 0.1),
+                  'l1_ratio': np.linspace(0, 1, 5)}
+# 파라미터 확인 
+ElasticNet().get_params()
+
+# 교차검증
+from sklearn.model_selection import KFold, GridSearchCV
+cv = KFold(n_splits=5, 
+           shuffle=True, 
+           random_state=2025)
+
+# 그리드서치
+elastic_search = GridSearchCV(estimator=elastic, 
+                              param_grid=elastic_params, 
+                              cv = cv, 
+                              scoring='neg_mean_squared_error')
+elastic_search.fit(X_train, y_train)
+
+pd.DataFrame(elastic_search.cv_results_)
+
+# best prameter
+print(elastic_search.best_params_)
+
+print(-elastic_search.best_score_)
+
+# elastic = ElasticNet(alpha=0.1, 
+#                      l1_ratio=0.75)
+# elastic.fit(X_train, y_train)
+# elastic.coef_
 
 # 테스트 데이터도 숫자형만 선택하고, 결측치는 평균으로 채움
 test_df = test_df.select_dtypes(include=['number'])
 test_df = test_df.fillna(train_df.mean())
 
 # 예측
-y_pred = elastic.predict(test_df)
+# y_pred = elastic.predict(test_df)
+y_pred = elastic_search.predict(test_df)
 submit = pd.read_csv('./data/houseprice/sample_submission.csv')
 submit["SalePrice"]=y_pred
 
