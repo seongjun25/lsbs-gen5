617d627465bd5a4b84b9ff66c729f513713de747
issactoast <issactoast@gmail.com>
2025-09-08 09:50:44 +0900

    dct classification

--- lec32-dct-classification.py
+++ lec32-dct-classification.py
@@ -27,3 +27,25 @@ sns.scatterplot(
 plt.title("Penguins (Gentoo vs Chinstrap)")
 plt.grid(True)
 plt.show()
+
+
+x = penguins_a[["bill_length_mm", "bill_depth_mm"]]
+y = penguins_a["species"]
+
+left_x = x.loc[x["bill_length_mm"] <= 50,:] 
+right_x = x.loc[x["bill_length_mm"] > 50,:] 
+left_y = y.loc[x["bill_length_mm"] <= 50]
+right_y = y.loc[x["bill_length_mm"] > 50]
+
+# 지니 인덱스
+특정 노드에 Gentoo만 있는 경우!
+특정 노드에 Chinstrap만 있는 경우!
+1 - (p_g^2 + p_c^2)
+1 - (1^2 + 0^2) = 1 - 1 = 0
+1 - (0^2 + 1^2) = 1 - 1 = 0
+
+특정 노드에 100 데이터 -> Gentoo 50개, Chinstrap 50개
+1 - (p_g^2 + p_c^2)
+1 - (0.5**2 + 0.5**2)
+
+
