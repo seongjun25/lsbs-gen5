411ec31b20dd61c0197071029f9764d51a3a4a3c
issactoast <issactoast@gmail.com>
2025-09-15 14:37:11 +0900

    pca 싱크

--- lec41-pca.py
+++ lec41-pca.py
@@ -13,3 +13,23 @@ df
 import seaborn as sns
 sns.pairplot(df, kind="scatter",
              diag_kind="hist");
+
+df.corr()
+
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(df)
+scaled_data = pd.DataFrame(scaled_data, 
+                           columns=df.columns)
+
+scaled_data
+
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=3)
+
+pca_array = pca.fit_transform(scaled_data)
+my_pca = pd.DataFrame(pca_array,
+                      index = scaled_data.index,
+                      columns=["pca1", "pca2", "pca3"])
